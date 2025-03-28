3d965d866714ea0c4304a3a5b40dad9ade683098
rtldg <rtldg@protonmail.com>
2025-03-28 12:22:50 +0000

    link to advapi32 for mimalloc

--- srcwrjson/src/lib.rs
+++ srcwrjson/src/lib.rs
@@ -1,11 +1,16 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-// Copyright 2022 rtldg <rtldg@protonmail.com>
+// Copyright 2022-2025 rtldg <rtldg@protonmail.com>
 // This file is part of srcwrtimer (https://github.com/srcwr/srcwrtimer/)
 
 #![allow(non_snake_case)]
 #![feature(let_chains)]
 
 mod natives_json;
+
+// Link to this because mimalloc calls `OpenProcessToken`, `AdjustTokenPrivileges`, and `LookupPrivilegeValueA` in `win_enable_large_os_pages`.
+#[cfg(target_family = "windows")]
+#[link(name = "advapi32")]
+unsafe extern "C" {}
 
 #[global_allocator]
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
